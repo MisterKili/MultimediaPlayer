a4d9a4fb0f0f0decb1a90888523e8d25e025ab4c
wiktor <wkilijan@gmail.com>
2021-03-19 21:07:12 +0100

    taking photos

--- app/src/main/java/com/example/multimediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/multimediaplayer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.multimediaplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var floatingButton: FloatingActionButton
+    private lateinit var filesRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        floatingButton = findViewById(R.id.floatingActionButton)
+        filesRecyclerView = findViewById(R.id.filesRecyclerView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        floatingButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
